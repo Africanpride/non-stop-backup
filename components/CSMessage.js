8b2756f1f353ca2b205e79abe8629c57a53fec12
pius <pius.opoku@pakatron.com>
2022-11-01 08:41:46 +0000

    solved livestream...

--- components/CSMessage.js
+++ components/CSMessage.js
@@ -66,16 +66,18 @@ const CSMessage = () => {
       className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-xl w-full h-full flex justify-center items-center z-40 overflow-hidden'>
 
       <div className='w-full md:w-auto h-4/6  z-50 grid place-content-center place-items-center  rounded-md inset-0 '>
-        <ReactPlayer
+        {/* <ReactPlayer
           className={'aspect-video w-full rounded-neat'}
-          url={'https://www.youtube.com/watch?v=PDn6LccF47c'}
+          url={'https://www.youtube.com/channel/UC_43aGdcd0mLYjeK84Yn3mg/live'}
           width='100%'
           height='100%'
           controls='true'
           // light='/images/john.jpg'
         playing='true'
         >
-        </ReactPlayer>
+        </ReactPlayer> */}
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/live_stream?channel=UC_43aGdcd0mLYjeK84Yn3mg" frameborder="0" allowfullscreen></iframe>
       </div>
       <div onClick={()=>{setShow(false)}} className='cursor-pointer absolute top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50'><FontAwesomeIcon icon={faTimesCircle} className="text-white text-3xl "></FontAwesomeIcon></div>
     </div>)}
